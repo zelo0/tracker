254aa1b36c591219a6c496751836cb8907f39195
zelo0 <minorSunbi@gmail.com>
2023-02-21 23:36:35 +0900

    fix: next-auth firebase adapter 최신 버전 모듈에 존재하는 import 버그로 인한 다운그레이드

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,21 +1,12 @@
 import NextAuth from 'next-auth';
 import GoogleProvider  from 'next-auth/providers/google';
-import { FirestoreAdapter, initFirestore } from "@next-auth/firebase-adapter"
-import { cert } from "firebase-admin/app"
+import { FirestoreAdapter } from "@next-auth/firebase-adapter"
+import { firebaseConfig } from "firebase.config.js"
 
 interface OauthClient {
   clientId: string,
   clientSecret: string
 }
-
-/* initFirestore: no duplicate app initialization issues in serverless environments */
-const firestore = initFirestore(({
-  credential: cert({
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
-  })
-}));
 
 export const authOptions = {
   providers: [
@@ -24,7 +15,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     } as OauthClient)
   ],
-  adapter: FirestoreAdapter(firestore),
+  adapter: FirestoreAdapter(firebaseConfig),
 };
 
 export default NextAuth(authOptions);
