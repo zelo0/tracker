43e163213d69ef9c1fa6327dc995c80d2e4ec31f
zelo0 <minorSunbi@gmail.com>
2023-03-13 00:20:46 +0900

    feat: title 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,22 @@ import '@/styles/global.scss'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import LoadingLayout from '@/components/layout/LoadingLayout';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={ session } >
-      <LoadingLayout>
-        <DefaultLayout>
-          <Component {...pageProps} />
-        </DefaultLayout>
-      </LoadingLayout>
-    </SessionProvider>
+    <>
+      <Head>
+        <title>가격 추적기</title>
+      </Head>
+
+      <SessionProvider session={ session } >
+        <LoadingLayout>
+          <DefaultLayout>
+            <Component {...pageProps} />
+          </DefaultLayout>
+        </LoadingLayout>
+      </SessionProvider>
+    </>
   );
 }
